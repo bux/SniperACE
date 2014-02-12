63f2c813d75472bb8662bae715137976abeaecfb
bux578 <github@jonathandavid.de>
2014-02-12 18:13:00 +0100

    removed ngRightClick directive
    It's not used anywhere

--- js/sniperApp.js
+++ js/sniperApp.js
@@ -1,17 +1,5 @@
 var app = angular.module('sniperApp', []);
 
-app.directive('ngRightClick', function($parse) {
-    return function(scope, element, attrs) {
-        var fn = $parse(attrs.ngRightClick);
-        element.bind('contextmenu', function(event) {
-            scope.$apply(function() {
-                event.preventDefault();
-                fn(scope, {$event:event});
-            });
-        });
-    };
-});
-
 app.directive('integer', function(){
     return {
         require: 'ngModel',
